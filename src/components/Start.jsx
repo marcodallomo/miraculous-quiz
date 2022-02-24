b0204500f490882772823e7fa2194386cb5da545
Marco Dall'Omo <marcodallomo@marcos-mbp.home>
2022-02-24 15:23:49 +0000

    modified css

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -16,8 +16,15 @@ const Start = ({ setUsername, setTopic }) => {
       <img src="images/ladybug2.jpg" alt="Miraculous" width="400" className="imageStart" />
       <input placeholder=" Enter your name" className="startInput" ref={inputRef} />
       <div className="topicChoice">
-        <input type="radio" name="choice" value="Math" onChange={handleChange} />  <label className="Math">MATH</label>
-        <input type="radio" name="choice" value="English" onChange={handleChange} />  <label className="English">ENGLISH</label>
+        <div>
+          <label className="English">ENGLISH</label>
+          <input type="radio" name="choice" value="English" onChange={handleChange} /> 
+        </div>
+
+        <div>
+          <label className="Math">MATH</label>
+          <input type="radio" name="choice" value="Math" onChange={handleChange} /> 
+        </div>
       </div>
 
       <button className="startButton" onClick={handleClick}>
